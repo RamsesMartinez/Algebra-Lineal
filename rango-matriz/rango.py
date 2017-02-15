6a6e089aad3af3fde2f334faf88411ba25d7d3e8
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-14 23:38:17 -0600

    Added gitignore

--- rango-matriz/rango.py
+++ rango-matriz/rango.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python
+"""
+Programa para hallar el rango de una matriz m x n
+donde m y n son numers enteros mayores o iguales a 1
+y menores o iguales a 8
+"""
 
-# Programa para hallar el rango de una matriz m x n
-# Donde m y n son números enteros, mayores o iguales a 1
-# 	y menores o iguales a 8
 
 class Matriz(object):
-	"""
-		Matriz con filas y n columnas con propiedad de rango
-	"""
-	def __init__(self):
-		self.filas = 1
-		self.columnas = 1
-		self.rango = 1
+    """
+    Matriz con filas y n columnas con propiedad de rango
+    """
 
-	def calcular_rango(self):
-		self.rango = 0
+    def __init__(self):
+        self.filas = []
+        self.columnas = []
+        self.rango = 1
 
-	def get_filas(self):
-		return self.filas
+    def calcular_rango(self):
+        self.rango = 0
 
-	def set_filas(self, filas):
-		self.filas = filas
+    def get_filas(self):
+        return self.filas
 
-	def get_columnas(self):
-		return self.columnas
+    def set_filas(self, filas):
+        self.filas = filas
 
-	def set_columnas(self, columnas):
-		self.columnas = columnas
+    def get_columnas(self):
+        return self.columnas
+
+    def set_columnas(self, columnas):
+      self.columnas = columnas
 
 
 def main():
@@ -40,4 +43,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+    main()
