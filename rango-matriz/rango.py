be4bc2c24625380ecfd78ecbfa3e9362ccfb786d
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-15 02:22:22 -0600

    Agregada la inicialización de la matriz. Creado el método para rellenar la matriz con validación de enteros y método para retornar la matriz completo.
    TODO: Crear un menú y el método final para calcula el rango.

--- rango-matriz/rango.py
+++ rango-matriz/rango.py
@@ -1,46 +1,78 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# -*- coding: <utf-8> -*-
 """
 Programa para hallar el rango de una matriz m x n
 donde m y n son numers enteros mayores o iguales a 1
 y menores o iguales a 8
 """
+__licence__ = "MIT"
+__author__ = "Ramśes Martínez Ortiz"
 
+
+from time import sleep
 
 class Matriz(object):
     """
-    Matriz con filas y n columnas con propiedad de rango
+    Matriz de mxn filas y columnas con propiedad de rango.
     """
 
     def __init__(self):
-        self.filas = []
-        self.columnas = []
-        self.rango = 1
+        self.__numero_filas = 1
+        self.__numero_columnas = 1
+        self.__matriz = None
 
     def calcular_rango(self):
-        self.rango = 0
+        self.__rango = 0
 
-    def get_filas(self):
-        return self.filas
+    def get_numero_filas(self):
+        return self.__numero_filas
 
-    def set_filas(self, filas):
-        self.filas = filas
+    def set_numero_filas(self, filas:int):
+        self.__numero_filas = filas
 
-    def get_columnas(self):
-        return self.columnas
+    def get_numero_columnas(self):
+        return self.__numero_columnas
 
-    def set_columnas(self, columnas):
-      self.columnas = columnas
+    def set_numero_columnas(self, columnas:int):
+        self.__numero_columnas = columnas
 
+    def set_matriz(self):
+        self.__matriz = [[None] * self.__numero_columnas for i in range(self.__numero_filas)]
+        for i in range(self.__numero_filas):
+            for j in range(self.__numero_columnas):
+                mensaje = 'Elemento de la celda (%s,%s):\t' % (i, j)
+                self.__matriz[i][j] = self.__get_numero(mensaje)
+    
+    def __get_numero(self, mensaje):
+        while True:
+            try:
+                return int(input(mensaje))
+            except ValueError:
+                print('Ingresa un número válido')
+        
+    def print_matriz(self):
+        for i in range(self.__numero_filas):
+            for j in range(self.__numero_columnas):
+                print('[%s,%s]' % (i+1, j+1), end='\t')    
+            print()
 
 def main():
-    print('Vamoa a Calcular el rango')
+    print('Vamoa a Calcular el rango de una matriz\n')
 
-    filas = input('Ingresa cantidad de filas: \t')
-    columnas = input('Ingrese cantidad de columnas: \t')
+    filas = int(input('Ingresa cantidad de filas: \t'))
+    columnas = int(input('Ingrese cantidad de columnas: \t'))
+
     matriz = Matriz()
-    matriz.set_filas(filas)
-    matriz.set_filas(columnas)
+    print()
+    matriz.set_numero_filas(filas)
+    matriz.set_numero_columnas(columnas)
+    matriz.set_matriz()
+    print('\nAsí quedó tu matriz:')
+    matriz.print_matriz()
 
+    print('\nCALCULANDO EL RANGO...')
+    sleep(1)
+    print('LISTO!')
 
 if __name__ == '__main__':
     main()
